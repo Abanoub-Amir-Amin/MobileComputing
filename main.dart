fed57dd21405dd0ebeb257fd4f6be84c76af80e1
Abanoub-Amir-Amin <73706242+Abanoub-Amir-Amin@users.noreply.github.com>
2023-03-23 00:05:18 +0200

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+      home: Scaffold(
+    appBar: AppBar(
+      title: const Text('First Application'),
+      backgroundColor: Colors.purple,
+    ),
+    body: const Center(
+      child: Text("Abanoub Amir Amin"),
+    ),
+  )));
+}
